9d7cd6ae7840574b525a5c7e15880f85173fcd0d
damu <damodar@acidgreen.com.au>
2014-04-10 17:29:30 +1000

    updated 404 log data

--- app/code/community/Technooze/T404/Model/Observer.php
+++ app/code/community/Technooze/T404/Model/Observer.php
@@ -7,28 +7,50 @@
  */
 class Technooze_T404_Model_Observer {
 
+    public function frontInitBefore(Varien_Event_Observer $observer){
+
+        return; // this can be enabled for quick debug.
+
+        $request = $observer->getEvent()->getFront()->getRequest();
+
+
+        $info = array(
+            'base_url' => Mage::getBaseUrl(),
+            'request_path' => $request->getOriginalPathInfo(),
+            'path' => $request->getPathInfo(),
+            'IP1' => $request->getClientIp(),
+            'IP2' => $request->getClientIp(false),
+            'params' => $request->getParams(),
+            'store' => Mage::app()->getStore()->getData(),
+        );
+        Mage::log($info);
+    }
+
     /**
      * 404 logger
      *
      * @param Varien_Event_Observer $observer
      */
     public function noRoute(Varien_Event_Observer $observer) {
-        $request = $observer->getEvent()->getControllerAction()->getRequest();
+        $event = $observer->getEvent();
+        $request = $event->getControllerAction()->getRequest();
         $actionName = $request->getActionName();
 
         if ($actionName == 'noRoute') {
             $requestUrl = rtrim($request->getScheme() . '://' . $request->getHttpHost() . $request->getRequestUri(), '/');
+
             $info = array(
+                'base_url' => Mage::getBaseUrl(),
                 'request_url' => $requestUrl,
+                'mage_url' => Mage::helper('core/url')->getCurrentUrl(),
+                'path' => $request->getPathInfo(),
                 'controller_name' => $request->getControllerName(),
                 'route_name' => $request->getRouteName(),
                 'module_name' => $request->getModuleName(),
-                'requested_route_name' => $request->getRequestedRouteName(),
-                'requested_controller_name' => $request->getRequestedControllerName(),
-                'requested_action_name' => $request->getRequestedActionName(),
                 'IP1' => $request->getClientIp(),
                 'IP2' => $request->getClientIp(false),
                 'params' => $request->getParams(),
+                'store' => Mage::app()->getStore()->getData(),
             );
 
             Mage::log($info, 7, '404-'.date("Ymd", time()).'.log', true);
