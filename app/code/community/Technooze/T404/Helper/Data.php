6e59d7e9b1f13176ea28e33746973a84e5230916
damu <damodar@acidgreen.com.au>
2014-04-15 13:17:06 +1000

    updated to remove store param from url and reload site.

--- app/code/community/Technooze/T404/Helper/Data.php
+++ app/code/community/Technooze/T404/Helper/Data.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @category   Technooze
- * @package    Technooze_T404
- * @author     Damodar Bashyal (github.com/dbashyal/Technooze_T404)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category    Technooze
+ * @package     Technooze_T404
+ * @author      Damodar Bashyal (github.com/dbashyal/Technooze_T404)
+ * @url         http://dltr.org (visit for more magento tips and tricks)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Technooze_T404_Helper_Data extends Mage_Core_Helper_Abstract
 {}
--- app/code/community/Technooze/T404/Model/Observer.php
+++ app/code/community/Technooze/T404/Model/Observer.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * @category   Technooze
- * @package    Technooze_T404
- * @author     Damodar Bashyal (github.com/dbashyal/Technooze_T404)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category    Technooze
+ * @package     Technooze_T404
+ * @author      Damodar Bashyal (github.com/dbashyal/Technooze_T404)
+ * @url         http://dltr.org (visit for more magento tips and tricks)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Technooze_T404_Model_Observer {
 
     public function frontInitBefore(Varien_Event_Observer $observer){
-
         return; // this can be enabled for quick debug.
 
         $request = $observer->getEvent()->getFront()->getRequest();
@@ -30,6 +30,7 @@ class Technooze_T404_Model_Observer {
      * 404 logger
      *
      * @param Varien_Event_Observer $observer
+     * @return Technooze_T404_Model_Observer
      */
     public function noRoute(Varien_Event_Observer $observer) {
         $event = $observer->getEvent();
@@ -38,11 +39,12 @@ class Technooze_T404_Model_Observer {
 
         if ($actionName == 'noRoute') {
             $requestUrl = rtrim($request->getScheme() . '://' . $request->getHttpHost() . $request->getRequestUri(), '/');
+            $mageUrl = Mage::helper('core/url')->getCurrentUrl();
 
             $info = array(
                 'base_url' => Mage::getBaseUrl(),
                 'request_url' => $requestUrl,
-                'mage_url' => Mage::helper('core/url')->getCurrentUrl(),
+                'mage_url' => $mageUrl,
                 'path' => $request->getPathInfo(),
                 'controller_name' => $request->getControllerName(),
                 'route_name' => $request->getRouteName(),
@@ -51,10 +53,33 @@ class Technooze_T404_Model_Observer {
                 'IP2' => $request->getClientIp(false),
                 'params' => $request->getParams(),
                 'store' => Mage::app()->getStore()->getData(),
+                'cookie' => Mage::getModel('core/cookie')->get('store'),
             );
 
             Mage::log($info, 7, '404-'.date("Ymd", time()).'.log', true);
+
+            /*
+             * reload site without param ___store
+             */
+            $this->redirect($mageUrl, $event->getControllerAction()->getResponse());
         }
-        return;
+        return $this;
+    }
+
+    /**
+     * @param string $url
+     * @param Mage_Core_Controller_Response_Http $response
+     * @return $this
+     */
+    public function redirect($url='', Mage_Core_Controller_Response_Http $response){
+        $storeParam     = Mage::app()->getRequest()->getParam('___store', false);
+        $storeCookie    = Mage::getModel('core/cookie')->get('store');
+        if($url || $storeParam || $storeCookie){
+            Mage::getModel('core/cookie')->delete('store');
+            $url = trim(str_replace("___store={$storeParam}", '', $url), '?');
+            $response->setRedirect($url)->sendResponse();
+            exit;
+        }
+        return $this;
     }
 }
